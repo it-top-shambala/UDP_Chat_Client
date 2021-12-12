56efd6a23f35af595205a8fcc6832fec20db7adc
Starinin Andrey <starininandrey@gmail.com>
2021-12-12 10:43:10 +0300

    Add UDP_Chat_Lib

---
+++ UDP_Chat_Lib/Udp.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace UDP_Chat_Lib
+{
+    public static class Udp
+    {
+        public static void SendBinary(byte[] message, string ip, int port)
+        {
+            using var sender = new UdpClient();
+            sender.Send(message, message.Length, ip, port);
+        }
+
+        public static byte[] ReceiveBinary(int port)
+        {
+            var receiver = new UdpClient(port);
+            IPEndPoint ip = null;
+            return receiver.Receive(ref ip);
+        }
+    }
+}
