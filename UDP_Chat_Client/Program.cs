bc1654653bbc3625e4d850ffd74e1b2a40b964c0
Starinin Andrey <starininandrey@gmail.com>
2021-12-12 11:11:20 +0300

    Update UDP_Chat_Client

--- UDP_Chat_Client/Program.cs
+++ UDP_Chat_Client/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using UDP_Chat_Lib;
 
 namespace UDP_Chat_Client
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string ip = string.Empty;
+        private static int port_remote = 0;
+        private static int port_local = 0;
+        static void Main()
         {
+            Console.Write("Введите удалённый ip-адрес: ");
+            ip = Console.ReadLine();
+            Console.Write("Введите удалённый порт: ");
+            port_remote = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите локальный порт: ");
+            port_local = Convert.ToInt32(Console.ReadLine());
             
+            Task.Run(Receive);
+
+            while (true)
+            {
+                Send();
+            }
+        }
+
+        static void Receive()
+        {
+            Udp.ReceiveBinary(port_local, out var data);
+            Console.WriteLine(Encoding.Unicode.GetString(data));
+        }
+
+        static void Send()
+        {
+            Console.Write("Введите сообщение: ");
+            var message = Console.ReadLine();
+            var data = Encoding.Unicode.GetBytes(message);
+            Udp.SendBinary(data, ip, port_remote);
         }
     }
 }
